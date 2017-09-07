715ed89aa02fcfc4cf7f8e67bd183c81b2cbf8e4
prokopiukd <prokopiukd@digicode.net>
2017-09-07 10:28:52 +0300

    Flink in cluster

---
+++ Apache Flink/flink-pluralsight-course/src/main/java/org/my/ch02/join/cluster/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.my.ch02.join.cluster;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+
+/**
+ *
+ * @author prokopiukd
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ParameterTool parameters = ParameterTool.fromArgs(args);
+    }
+    
+}
