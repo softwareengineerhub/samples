237854414700cf8c6a8d653ba520aee46cea4e97
prokopiukd <prokopiukd@digicode.net>
2017-09-14 11:51:23 +0300

    Insertion sort

--- Algorithm/Algorithm/src/main/java/sorting/impl/insertion/InsertionSorter.java
+++ Algorithm/Algorithm/src/main/java/sorting/impl/insertion/InsertionSorter.java
@@ -5,7 +5,9 @@
  */
 package sorting.impl.insertion;
 
+import java.util.Arrays;
 import sorting.Sorter;
+import sorting.impl.bubble.BubbleSorter;
 
 /**
  *
@@ -15,12 +17,23 @@ public class InsertionSorter implements Sorter {
 
     @Override
     public void sort(int[] data) {
-        for(int i=0;i<data.length;i++){
-            int k=i;
-            while(k>0){
-                
+        for (int i = 0; i < data.length - 1; i++) {
+            int k = i + 1;
+            while (k > 0 && data[k] > data[k - 1]) {
+                data[k] = data[k] + data[k - 1];
+                data[k - 1] = data[k] - data[k - 1];
+                data[k] = data[k] - data[k - 1];
+                k--;
             }
+            System.out.println(i + ". " + Arrays.toString(data));
         }
     }
-    
+
+    public static void main(String[] args) {
+        Sorter sorter = new InsertionSorter();
+        int[] data = {1, 9, 0, -10, 45, 37, 9, 100, -5};
+        sorter.sort(data);
+        System.out.println(Arrays.toString(data));
+    }
+
 }
