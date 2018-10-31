a1971e18c5a034ba308eb106c68a4cf782b0916d
denis4321 <denis4321@ukr.net>
2018-10-31 09:58:10 +0300

    mavenproject3

---
+++ Struts/mavenproject3/src/main/java/com/simpleprogrammer/actions/HelloAction.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.simpleprogrammer.actions;
+
+import com.opensymphony.xwork2.Action;
+
+/**
+ *
+ * @author asusadmin
+ */
+public class HelloAction implements Action {
+
+    private String greeting;
+
+    @Override
+    public String execute() throws Exception {
+        greeting = "Hello Struts 2";
+        return "success";
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+}
