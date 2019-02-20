cca478abd9bc2884394c48d8c8bccb05c759f899
denis4321 <denis4321@ukr.net>
2019-02-20 12:56:18 +0200

    DoubleCheckSingleton

---
+++ VolatileExample/src/main/java/org/app/DclSingleton.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.app;
+
+/**
+ *
+ * @author Denys.Prokopiuk
+ */
+public class DclSingleton {
+
+    private static volatile DclSingleton instance;
+
+    public static DclSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DclSingleton.class) {
+                if (instance == null) {
+                    instance = new DclSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
